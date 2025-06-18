6115aa491802790282d55b3e6f86a614de605bfc
giapvanchien070801 <chien.giap@netko-solution.com>
2025-06-19 02:33:08 +0700

    [Feat] - [UI version 2]

--- src/app/(user)/layout.js
+++ src/app/(user)/layout.js
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="shortcut icon" href="/images/LOGO.JPG" />
         <title>Hai Duyen Bio</title>
       </head>
       <body>
--- src/components/user/UserHeader.js
+++ src/components/user/UserHeader.js
@@ -7,13 +7,19 @@ import {
   FacebookFilled,
   MenuOutlined,
   ShoppingOutlined,
+  RightOutlined,
+  HomeOutlined,
+  InfoCircleOutlined,
+  FileTextOutlined,
+  PlayCircleOutlined,
+  ContactsOutlined,
 } from "@ant-design/icons";
 import Link from "next/link";
 import { useQuery } from "react-query";
 import Base from "@/models/Base";
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
-import { Button, Popover, Tooltip } from "antd";
+import { Badge, Button, Popover, Tooltip } from "antd";
 
 export default function UserHeader() {
   const pathname = usePathname();
@@ -122,181 +128,174 @@ export default function UserHeader() {
 
   return (
     <header className="z-20 relative">
-      <div
-        className={`${layoutUserStyle.background_info_top} z-20 hidden md:block`}
-      >
-        <div className="container mx-auto text-white flex justify-between">
-          <div className="flex items-center">
-            <p className="px-2 flex">
+      <div className={` z-20 hidden md:block`}>
+        <div className="container mx-auto  flex justify-between py-5">
+          <div className="flex items-center gap-5">
+            <Link href={`/`} className="lg:hidden xl:block">
+              <div className="flex items-center">
+                <img src="/images/logo-haiduyenbio-1.png" className=" h-14" />
+              </div>
+            </Link>
+            <p className="flex">
               <PhoneFilled /> <span className="mx-2">085.489.1993</span>
             </p>
-            <p className="px-2 flex">
+            <p className="flex">
               <MailFilled /> <span className="mx-2">HaiDuyenBIO@gmail.com</span>
             </p>
           </div>
 
-          <div className="flex items-center">
-            <Link
-              href={`https://www.facebook.com/profile.php?id=100057086214537&mibextid=ZbWKwL`}
-              className="text-lg p-2 flex hover:bg-cyan-400"
-            >
-              <FacebookFilled />
+          
+            <Link href={`/shopping/step1`} className="hover:bg-cyan-400 flex items-center gap-7 p-2">
+              <Badge count={5}>
+                <Tooltip title="Giỏ hàng">
+                  <ShoppingOutlined className="text-4xl cursor-pointer flex  " />
+                </Tooltip>
+              </Badge>
+              <div>
+                <p className=" text-sm font-medium text-[#777]">Giỏ hàng</p>
+                <p className=" text-xs underline">0đ</p>
+              </div>
             </Link>
-            <Link
-              href={`https://zalo.me/0867585366`}
-              className="text-lg m-2 p-1 flex hover:bg-cyan-400"
-            >
-              Zalo
-            </Link>
-
-            <Link href={`/shopping/step1`}>
-              <Tooltip title="Giỏ hàng">
-                <ShoppingOutlined className="text-2xl p-4 cursor-pointer flex hover:bg-cyan-400 animate-bounce hover:animate-none" />
-              </Tooltip>
-            </Link>
-          </div>
+         
         </div>
       </div>
 
-      <div className="container mx-auto flex justify-between items-center z-20 bg-white py-4 lg:px-0 px-4">
-        <Link href={`/`} className="lg:hidden xl:block">
+      <div className="  bg-[#14457b] py-4 lg:px-0 px-4 flex justify-center">
+        {/* <Link href={`/`} className="lg:hidden xl:block">
           <div className="flex items-center">
             <img src="/images/logo-haiduyenbio-1.png" className=" h-14" />
           </div>
-        </Link>
+        </Link> */}
 
-        <div className="flex justify-between lg:w-full xl:w-fit">
-          <div className="navbar self-stretch lg:static absolute bg-white z-10 top-full w-full lg:w-fit left-0">
+        <div className="flex  items-center  justify-between w-3/4 text-white">
+          <Popover
+            content={
+              <ul className="w-max bg-white">
+                <li>
+                  <Popover
+                    content={
+                      <ul className="w-max bg-white">
+                        {listSocial?.map((social, index) => (
+                          <li key={index} className="w-full">
+                            {/* link đến tranh danh sách bài viết */}
+                            <Link
+                              href={`/product-detail/${social?.id}`}
+                              as={`/product-detail/${social?.id}`}
+                              className="hover:text-white block hover:bg-cyan-600 py-2 px-8 transition-all duration-300 lg:px-4 lg:py-2 rounded">
+                              {social?.name}
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    }
+                    trigger="hover"
+                    placement="right">
+                    <Link
+                      href={`#`}
+                      className="h-full flex items-center hover:text-cyan-600 transition-all duration-300 p-2 w-full justify-between gap-4">
+                      Sản phẩm
+                      <RightOutlined />
+                    </Link>
+                  </Popover>
+                </li>
+
+                <li>
+                  <Popover
+                    content={
+                      <div className="flex gap-4">
+                        {menu2?.map((menu, index) => (
+                          <ul className="w-max bg-white">
+                            <li className="text-lg font-bold">{menu?.menu}</li>
+                            {menu?.submenu?.map((submenu, index) => (
+                              <li key={index} className="w-full">
+                                <Link
+                                  href={`/news/${submenu?.id}`}
+                                  as={`/news/${submenu?.id}`}
+                                  className="hover:text-white block hover:bg-cyan-600 py-2 px-8 transition-all duration-300 lg:px-4 lg:py-2 rounded">
+                                  {submenu?.name}
+                                </Link>
+                              </li>
+                            ))}
+                          </ul>
+                        ))}
+                      </div>
+                    }
+                    trigger="hover"
+                    placement="right">
+                    <Link
+                      href={`#`}
+                      className="h-full flex items-center hover:text-cyan-600 transition-all duration-300 p-2 w-full justify-between gap-4">
+                      Nguyên liệu vi sinh
+                      <RightOutlined />
+                    </Link>
+                  </Popover>
+                </li>
+
+                <li>
+                  <Popover
+                    content={
+                      <ul className="w-max bg-white">
+                        {listSocial?.map((social, index) => (
+                          <li key={index} className="w-full">
+                            {/* link đến tranh danh sách bài viết */}
+                            <Link
+                              href={`/news/${social?.id}`}
+                              as={`/news/${social?.id}`}
+                              className="hover:text-white block hover:bg-cyan-600 py-2 px-8 transition-all duration-300 lg:px-4 lg:py-2 rounded">
+                              {social?.name}
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    }
+                    trigger="hover"
+                    placement="right">
+                    <Link
+                      href={`#`}
+                      className="h-full flex items-center  hover:text-cyan-600 transition-all duration-300 p-2 w-full justify-between gap-4">
+                      Gia công vi sinh
+                      <RightOutlined />
+                    </Link>
+                  </Popover>
+                </li>
+              </ul>
+            }
+            trigger="hover"
+            placement="bottom">
+            <div className="flex items-center gap-3 cursor-pointer">
+              <MenuOutlined className=" text-2xl" />
+              <p>MAIN MENU</p>
+              <DownOutlined />
+            </div>
+          </Popover>
+
+          <div className=" self-stretch lg:static absolute  z-10 top-full w-full lg:w-fit left-0">
             <ul
               className={`lg:flex items-center h-full md:container md:mx-auto lg:p-0 p-4 gap-2 ${
                 activeMobileMenu ? "lg:block" : "hidden lg:block"
-              }`}
-            >
+              }`}>
               <li className="h-full relative">
                 <Link
                   href={`/`}
-                  className="h-full flex items-center p-2 hover:text-cyan-600 transition-all duration-300 py-2"
-                >
+                  className="h-full flex items-center p-2 hover:text-cyan-600 transition-all duration-300 py-2">
+                  <HomeOutlined className="mr-2" />
                   <p>Trang chủ</p>
                 </Link>
               </li>
               <li className="h-full relative">
                 <Link
                   href={`/about`}
-                  className="h-full flex items-center p-2 hover:text-cyan-600 transition-all duration-300 py-2"
-                >
+                  className="h-full flex items-center p-2 hover:text-cyan-600 transition-all duration-300 py-2">
+                  <InfoCircleOutlined className="mr-2" />
                   <p>Giới thiệu</p>
                 </Link>
-              </li>
-
-              <li>
-                <Popover
-                  content={
-                    <ul className="w-max bg-white">
-                      {listSocial?.map((social, index) => (
-                        <li key={index} className="w-full">
-                          {/* link đến tranh danh sách bài viết */}
-                          <Link
-                            href={`/product-detail/${social?.id}`}
-                            as={`/product-detail/${social?.id}`}
-                            className="hover:text-white block hover:bg-cyan-600 py-2 px-8 transition-all duration-300 lg:px-4 lg:py-2 rounded"
-                          >
-                            {social?.name}
-                          </Link>
-                        </li>
-                      ))}
-                    </ul>
-                  }
-                  trigger="hover"
-                  placement="bottom"
-                >
-                  <Link
-                    href={`#`}
-                    className="h-full flex items-center hover:text-cyan-600 transition-all duration-300 p-2"
-                  >
-                    Sản phẩm
-                    <DownOutlined
-                      style={{ fontSize: "14px", marginLeft: "5px" }}
-                    />
-                  </Link>
-                </Popover>
-              </li>
-
-              <li>
-                <Popover
-                  content={
-                    <div className="flex gap-4">
-                      {menu2?.map((menu, index) => (
-                        <ul className="w-max bg-white">
-                          <li className="text-lg font-bold">{menu?.menu}</li>
-                          {menu?.submenu?.map((submenu, index) => (
-                            <li key={index} className="w-full">
-                              <Link
-                                href={`/news/${submenu?.id}`}
-                                as={`/news/${submenu?.id}`}
-                                className="hover:text-white block hover:bg-cyan-600 py-2 px-8 transition-all duration-300 lg:px-4 lg:py-2 rounded"
-                              >
-                                {submenu?.name}
-                              </Link>
-                            </li>
-                          ))}
-                        </ul>
-                      ))}
-                    </div>
-                  }
-                  trigger="hover"
-                  placement="bottom"
-                >
-                  <Link
-                    href={`#`}
-                    className="h-full flex items-center hover:text-cyan-600 transition-all duration-300 p-2"
-                  >
-                    Nguyên liệu vi sinh
-                    <DownOutlined
-                      style={{ fontSize: "14px", marginLeft: "5px" }}
-                    />
-                  </Link>
-                </Popover>
-              </li>
-
-              <li>
-                <Popover
-                  content={
-                    <ul className="w-max bg-white">
-                      {listSocial?.map((social, index) => (
-                        <li key={index} className="w-full">
-                          {/* link đến tranh danh sách bài viết */}
-                          <Link
-                            href={`/news/${social?.id}`}
-                            as={`/news/${social?.id}`}
-                            className="hover:text-white block hover:bg-cyan-600 py-2 px-8 transition-all duration-300 lg:px-4 lg:py-2 rounded"
-                          >
-                            {social?.name}
-                          </Link>
-                        </li>
-                      ))}
-                    </ul>
-                  }
-                  trigger="hover"
-                  placement="bottom"
-                >
-                  <Link
-                    href={`#`}
-                    className="h-full flex items-center hover:text-cyan-600 transition-all duration-300 p-2"
-                  >
-                    Gia công vi sinh
-                    <DownOutlined
-                      style={{ fontSize: "14px", marginLeft: "5px" }}
-                    />
-                  </Link>
-                </Popover>
               </li>
 
               <li className="h-full relative">
                 <Link
                   href={`/news`}
-                  className="h-full flex items-center p-2 hover:text-cyan-600 transition-all duration-300 py-2"
-                >
+                  className="h-full flex items-center p-2 hover:text-cyan-600 transition-all duration-300 py-2">
+                  <FileTextOutlined className="mr-2" />
                   <p>Tin tức</p>
                 </Link>
               </li>
@@ -304,28 +303,20 @@ export default function UserHeader() {
               <li className="h-full relative">
                 <Link
                   href={`/list-videos`}
-                  className="h-full flex items-center hover:text-cyan-600 transition-all duration-300 p-2"
-                >
+                  className="h-full flex items-center hover:text-cyan-600 transition-all duration-300 p-2">
+                  <PlayCircleOutlined className="mr-2" />
                   Video
                 </Link>
               </li>
+              <li className="h-full relative">
+                <Link
+                  href={`/contact`}
+                  className="h-full flex items-center hover:text-cyan-600 transition-all duration-300 p-2">
+                  <ContactsOutlined className="mr-2" />
+                  Liên hệ
+                </Link>
+              </li>
             </ul>
-          </div>
-
-          <Link
-            href={`/contact`}
-            className={`transition-all ml-2 duration-300 flex items-center lg:p-4 lg:mr-2 p-2 py-4 text-white rounded-md hover:bg-[#14457b] !bg-[#2cb1ab]`}
-          >
-            Liên hệ +
-          </Link>
-
-          <div className="search flex lg:hidden block">
-            <button
-              className="block lg:hidden p-4 session_ocean2 rounded-md text-white ml-2"
-              onClick={() => toggleMenuMobile()}
-            >
-              <MenuOutlined />
-            </button>
           </div>
         </div>
       </div>
